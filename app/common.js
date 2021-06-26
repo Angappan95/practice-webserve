1aa72cd2512023f9e23a68b19f6be22a9febb520
Angappan95 <angappan.ganesan@walmart.com>
2021-06-26 11:02:10 +0530

    Commiting ts check

--- app/common.js
+++ app/common.js
@@ -1,3 +1,7 @@
+//@ts-check
 const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const isGreater = (a, b) => (a > b ? a : b);
 
-export default addition;
+export { addition, subtraction, mul as multiplication, isGreater as grt };
--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
-import addition from "./common";
+//@ts-check
+import { addition, isGreater } from "./common";
 
 let sum = addition(2, 3);
+let greater = isGreater(10, 15);
 console.log(sum);
+console.log(greater);
