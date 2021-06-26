86afa431e421efc206581ac07603dc3e3633dd0f
Angappan95 <angappan.ganesan@walmart.com>
2021-06-26 10:53:05 +0530

    Testing webpack solution

--- app/common.js
+++ app/common.js
@@ -1,3 +1,6 @@
 const addition = (a, b) => a + b;
+const subraction = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const division = (a, b) => a / b;
 
-export default addition;
+export { addition, subraction, mul as multiplication, division };
--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
-import addition from "./common";
+import { addition, mul } from "./common";
 
 let sum = addition(2, 3);
+let product = mul(10, 3.5);
 console.log(sum);
+console.log(product);
